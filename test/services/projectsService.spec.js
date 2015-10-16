076b63c6e383f86d07c8350caed8cb6fa56dce6a
Artem Demo <artemdemo@gmail.com>
2015-10-17 00:35:44 +0300

    Added projectService.spec.js

--- test/services/projectsService.spec.js
+++ test/services/projectsService.spec.js
@@ -1,42 +1,43 @@
 describe('projectsService', function(){
 
-    var $rootScope,
-        projectsService;
-
+    let $rootScope;
+    let projectsService;
 
     beforeEach(module('pmApp'));
 
 
-    beforeEach(inject(function (_projectsService_, _$rootScope_) {
+    beforeEach(inject((_projectsService_, _$rootScope_) => {
         projectsService = _projectsService_;
         $rootScope = _$rootScope_;
     }));
 
 
-    describe('Empty task object', function(){
-        it('name is empty', function(){ expect(projectsService.getEmptyProject().name).toBe('') });
-        it('sp is null', function(){ expect(projectsService.getEmptyProject().sp).toBe(null) });
-        it('description is empty', function(){ expect(projectsService.getEmptyProject().description).toBe('') });
+    describe('Empty task object', () => {
+        it('name is empty', () => expect(projectsService.getEmptyProject().name).toBe(''));
+        it('sp is null', () => expect(projectsService.getEmptyProject().sp).toBe(null));
+        it('description is empty', () => expect(projectsService.getEmptyProject().description).toBe(''));
 
-        var date = moment(new Date());
-        it('created_at.date has current date in format YYYY-MM-DD', function(){ expect(projectsService.getEmptyProject().created_at.date).toBe(date.format('YYYY-MM-DD')) });
-        it('created_at.time has current time in format HH:mm', function(){ expect(projectsService.getEmptyProject().created_at.time).toBe(date.format('HH:mm')) });
+        let date = moment(new Date());
+        it(
+            'created_at.date has current date in format YYYY-MM-DD',
+            () => expect(projectsService.getEmptyProject().created_at.date).toBe(date.format('YYYY-MM-DD'))
+        );
+        it(
+            'created_at.time has current time in format HH:mm',
+            () => expect(projectsService.getEmptyProject().created_at.time).toBe(date.format('HH:mm'))
+        );
     });
 
 
-    describe('By default projects variable when resolved', function(){
-        var projects;
+    describe('By default projects variable when resolved', () => {
+        let projects;
 
-        beforeEach(function(){
-            projectsService.getProjects().then(function(response){
-                projects = response
-            });
+        beforeEach(() => {
+            projectsService.getProjects().then((response) => projects = response);
             $rootScope.$digest();
         });
 
-        it('is empty array', function(){
-            expect(projects).toEqual([]);
-        });
+        it('is empty array', () => expect(projects).toEqual([]));
 
     });
 });
