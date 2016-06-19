070ca75fc3f5906ed437de5a0a48620e6b791356
Artem Demo <artemdemo@gmail.com>
2016-06-19 21:40:00 +0300

    Removed usage of deprecated provide()
    Now you pass LocationStrategy within simole object withou provide()

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -4,7 +4,7 @@ import 'zone.js/dist/zone';
 import 'reflect-metadata';
 
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {provide, ComponentRef} from '@angular/core';
+import {ComponentRef} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {LocationStrategy, PathLocationStrategy} from '@angular/common';
@@ -20,7 +20,7 @@ import {appInjector} from './services/appInjector';
 bootstrap(MainRouter, [
     HTTP_PROVIDERS,
     ROUTER_PROVIDERS,
-    provide(LocationStrategy, {useClass: PathLocationStrategy}),
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
     AuthorizationService,
     TasksService,
     SelectedTaskService,
