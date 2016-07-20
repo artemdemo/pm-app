2abd911aaa7dc1bc6c9d297f90d13fe4045633e1
Artem Demo <artemdemo@gmail.com>
2016-07-20 20:22:18 +0300

    removed typings files

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-
-/// <reference path="es6-shim/es6-shim.d.ts" />
-/// <reference path="requirejs/require.d.ts" />
-/// <reference path="moment/moment-node.d.ts" />
-/// <reference path="moment/moment.d.ts" />
