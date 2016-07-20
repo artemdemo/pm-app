1e2b617301b0a5f7802a7fb26c34b3d056dc768e
Artem Demo <artemdemo@gmail.com>
2016-07-20 23:37:44 +0300

    added task filter by 'updated'

--- app/reducers/tasks.js
+++ app/reducers/tasks.js
@@ -1,9 +1,31 @@
 import * as tasksConst from '../constants/tasks';
 
+function sortTasksByUpdate(inTasks = []) {
+    const sortedByUpdate = inTasks.sort((taskA, taskB) => {
+        if (taskA.updated > taskB.updated) {
+            return -1;
+        }
+        if (taskA.updated < taskB.updated) {
+            return 1;
+        }
+        return 0;
+    });
+    const undoneTasks = [];
+    const doneTasks = [];
+    sortedByUpdate.forEach((task) => {
+        if (task.done) {
+            doneTasks.push(task);
+        } else {
+            undoneTasks.push(task);
+        }
+    });
+    return undoneTasks.concat(doneTasks);
+}
+
 export default function tasks(state = [], action) {
     switch (action.type) {
         case tasksConst.TASKS_LOADED:
-            return action.tasks;
+            return sortTasksByUpdate(action.tasks);
         case tasksConst.TASKS_ADDED:
             return [action.task].concat(state);
         case tasksConst.TASK_DELETED:
@@ -19,11 +41,11 @@ export default function tasks(state = [], action) {
         case tasksConst.TASK_UPDATED:
             for (let i = 0, len = state.length; i < len; i++) {
                 if (state[i].id === action.task.id) {
-                    return [
+                    return sortTasksByUpdate([
                         ...state.slice(0, i),
                         action.task,
                         ...state.slice(i + 1),
-                    ];
+                    ]);
                 }
             }
             return state;
