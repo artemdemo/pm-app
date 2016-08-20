f5572dd1b8f073f5f3f39f53b631fd5ce2bdafe6
Artem Demo <artemdemo@gmail.com>
2016-08-20 16:22:16 +0300

    fadeIn animation for modal

--- app/components/Modal/Modal.jsx
+++ app/components/Modal/Modal.jsx
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
 import * as modalConst from '../../constants/modal';
+import { hideModal } from '../../actions/modal';
 
 import './Modal.less';
 
+const MODAL_BG_CLASS = 'pm-modal-bg';
+
 class Modal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.bgClick = (e) => {
+            const { hideModal } = this.props;
+            if (e.target.className === MODAL_BG_CLASS) {
+                hideModal();
+            }
+        };
+    }
+
     render() {
         const { modal } = this.props;
+        const bgClass = classnames({
+            [MODAL_BG_CLASS]: true,
+            'pm-modal-bg_fadeIn': true,
+        });
+
+        const modalClass = classnames({
+            'pm-modal': true,
+            'pm-modal_fadeIn': true,
+        });
 
         if (modal.type === modalConst.SHOW_MODAL) {
             return (
-                <div className='pm-modal-bg'>
-                    <div className='pm-modal'>
+                <div className={bgClass} onClick={this.bgClick}>
+                    <div className={modalClass}>
                         {modal.content}
                     </div>
                 </div>
@@ -27,5 +51,7 @@ export default connect(
         return {
             modal: state.modal,
         };
+    }, {
+        hideModal,
     }
 )(Modal);
---
+++ app/styles/animated/readme.md
@@ -0,0 +1,3 @@
+# CSS animations
+
+All animation is based on [animation.css](https://daneden.github.io/animate.css/)
