1088f64d93bb34d0204426d25a9b34686373a9cc
Artem Demo <artemdemo@gmail.com>
2018-01-12 19:51:06 +0200

    removed pre-push

--- postinstall.sh
+++ postinstall.sh
@@ -2,7 +2,6 @@
 
 # copying hooks
 cp -i hooks/pre-commit.sh .git/hooks/pre-commit
-cp -i hooks/pre-push.sh .git/hooks/pre-push
 
 # installing last chrome driver
 node_modules/selenium-standalone/bin/selenium-standalone install
