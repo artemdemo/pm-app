ead73c770c1f0d93b13388f035e348ac0d9f9b69
Artem Demo <artemdemo@gmail.com>
2018-01-13 10:13:37 +0200

    refactoring model/projects

--- server/models/projects.js
+++ server/models/projects.js
@@ -1,12 +1,10 @@
-const debug = require('debug')('pm:models:projects');
 const moment = require('moment');
 const DB = require('sqlite-crud');
 const sessions = require('./sessions');
-const errConstants = require('../constants/error');
 
 const tableName = 'projects';
 
-exports.getAll = projectsData => new Promise((resolve, reject) => {
+const getAll = async function(projectsData) {
     const projectsQuery = `SELECT projects.id, projects.name, projects.description,
                                   projects.added, projects.updated
                            FROM projects
@@ -14,70 +12,59 @@ exports.getAll = projectsData => new Promise((resolve, reject) => {
                                    ON sessions.user_id = projects.user_id
                            WHERE sessions.id = ?;`;
 
-    DB.queryRows(projectsQuery, [projectsData.tokenId])
-        .then((projects) => {
-            const promisesList = [];
-            projects.forEach((project) => {
-                const tasksQuery = `SELECT projects_tasks_relations.task_id,
+    const projects = await DB.queryRows(projectsQuery, [projectsData.tokenId]);
+    const promisesList = [];
+    projects.forEach((project) => {
+        const tasksQuery = `SELECT projects_tasks_relations.task_id,
                                            projects_tasks_relations.project_id
                                     FROM projects
                                     INNER JOIN projects_tasks_relations
                                             ON projects.id = projects_tasks_relations.project_id
                                     WHERE projects.id = ?;`;
-                promisesList.push(DB.queryRows(tasksQuery, [project.id]));
-            });
-            return Promise.all(promisesList)
-                .then((resultsList) => {
-                    resultsList.forEach((data, index) => {
-                        projects[index].tasks = data.map(item => item.task_id);
-                    });
-                    resolve(projects);
-                });
-        })
-        .catch((err) => {
-            debug(new Error(err));
-            reject(errConstants.DB_ERROR);
-        });
-});
+        promisesList.push(DB.queryRows(tasksQuery, [project.id]));
+    });
+
+    const resultsList = await Promise.all(promisesList);
+    resultsList.forEach((data, index) => {
+        projects[index].tasks = data.map(item => item.task_id);
+    });
+    return projects;
+};
 
 
-exports.addNew = (newProjectData) => {
+const addNew = async function(newProjectData) {
     const now = moment(new Date());
 
     if (!newProjectData.payload.name) {
-        const err = 'No newProjectData.payload.name in given project';
-        debug(new Error(err));
-        return Promise.reject(err);
+        throw new Error('No newProjectData.payload.name in given project');
     }
 
     const id = newProjectData.tokenId;
 
-    return sessions.getSession({ id })
-        .then((session) => {
-            return DB.insertRow(tableName, {
-                name: newProjectData.payload.name,
-                description: newProjectData.payload.description,
-                added: now.format('YYYY-MM-DD HH:mm:ss'),
-                updated: now.format('YYYY-MM-DD HH:mm:ss'),
-                user_id: session.user_id,
-            });
-        })
-        .then(result => ({
-            id: result.id,
-            added: now.format('YYYY-MM-DD HH:mm:ss'),
-            updated: now.format('YYYY-MM-DD HH:mm:ss'),
-        }));
+    const session = await sessions.getSession({ id });
+    const result = await DB.insertRow(tableName, {
+        name: newProjectData.payload.name,
+        description: newProjectData.payload.description,
+        added: now.format('YYYY-MM-DD HH:mm:ss'),
+        updated: now.format('YYYY-MM-DD HH:mm:ss'),
+        user_id: session.user_id,
+    });
+
+    return {
+        id: result.id,
+        added: now.format('YYYY-MM-DD HH:mm:ss'),
+        updated: now.format('YYYY-MM-DD HH:mm:ss'),
+    };
 };
 
-exports.updateProject = (projectData) => {
+
+const updateProject = async function(projectData) {
     const now = moment(new Date());
     const updateData = {};
     const updateAllowed = true;
 
     if (!projectData.payload.id) {
-        const err = 'No projectData.payload.id in given project';
-        debug(new Error(err));
-        return Promise.reject(err);
+        throw new Error('No projectData.payload.id in given project');
     }
 
     const allowedFields = ['name', 'description'];
@@ -88,52 +75,52 @@ exports.updateProject = (projectData) => {
     });
 
     if (!updateAllowed) {
-        const err = 'No fields to update';
-        debug(new Error(err));
-        debug(projectData.payload);
-        return Promise.reject(err);
+        throw new Error('No fields to update');
     }
 
     updateData.updated = now.format('YYYY-MM-DD HH:mm:ss');
 
     const id = projectData.tokenId;
 
-    return sessions.getSession({ id })
-        .then((session) => {
-            return DB.updateRow(tableName, updateData, [{
-                column: 'id',
-                comparator: '=',
-                value: projectData.payload.id,
-            }, {
-                column: 'user_id',
-                comparator: '=',
-                value: session.user_id,
-            }]);
-        })
-        .then(() => ({
-            updated: updateData.updated,
-        }));
+    const session = await sessions.getSession({ id });
+    await DB.updateRow(tableName, updateData, [{
+        column: 'id',
+        comparator: '=',
+        value: projectData.payload.id,
+    }, {
+        column: 'user_id',
+        comparator: '=',
+        value: session.user_id,
+    }]);
+
+    return {
+        updated: updateData.updated,
+    };
 };
 
-exports.deleteProject = (projectData) => {
+
+const deleteProject = async function(projectData) {
     if (!projectData.payload) {
-        const err = 'No projectId in given project';
-        debug(new Error(err));
-        return Promise.reject(err);
+        throw new Error('No projectId in given project');
     }
 
     const id = projectData.tokenId;
 
-    return sessions.getSession({ id })
-        .then((session) => {
-            return DB.deleteRows(tableName, [{
-                column: 'id',
-                comparator: '=',
-                value: projectData.payload,
-            }, {
-                column: 'user_id',
-                comparator: '=',
-                value: session.user_id,
-            }]);
-        });
+    const session = await sessions.getSession({ id });
+    return DB.deleteRows(tableName, [{
+        column: 'id',
+        comparator: '=',
+        value: projectData.payload,
+    }, {
+        column: 'user_id',
+        comparator: '=',
+        value: session.user_id,
+    }]);
+};
+
+module.exports = {
+    getAll,
+    addNew,
+    updateProject,
+    deleteProject,
 };
