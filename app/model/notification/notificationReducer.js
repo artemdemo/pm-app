29237c4124eefe4d90efda3af9529bb49eb733a4
Artem Demo <artemdemo@gmail.com>
2018-01-20 16:08:39 +0200

    refactored notifications

--- app/model/notification/notificationReducer.js
+++ app/model/notification/notificationReducer.js
@@ -1,18 +1,22 @@
 import * as notificationConst from './notificationConst';
 
-export default function notification(state = {}, action) {
+const initialState = {
+    type: notificationConst.HIDE_MSG,
+    message: '',
+};
+
+export default function notification(state = initialState, action) {
     switch (action.type) {
         case notificationConst.ERROR_MSG:
         case notificationConst.SUCCESS_MSG:
-            return {
+            return Object.assign({}, state, {
                 type: action.type,
                 message: action.message,
-            };
+            });
         case notificationConst.HIDE_MSG:
-            return {
+            return Object.assign({}, state, {
                 type: action.type,
-                message: state.message,
-            };
+            });
         default:
             return state;
     }
