9b0808dda165b48dc4d296145ffd7adae59b2264
Artem Demo <artemdemo@gmail.com>
2018-05-11 22:34:14 +0300

    simplified projects reducer

--- app/source/model/projects/projectsReducer.js
+++ app/source/model/projects/projectsReducer.js
@@ -1,6 +1,6 @@
 import * as projectsConst from './projectsConst';
 
-const initialState = {
+const initState = {
     data: [],
     loading: false,
     loadingError: null,
@@ -12,89 +12,98 @@ const initialState = {
     deletingError: null,
 };
 
-export default function projects(state = initialState, action) {
+export default function projects(state = initState, action) {
     switch (action.type) {
         /*
-         * Loading
+         * Load
          */
         case projectsConst.LOAD_PROJECTS:
-            return Object.assign({}, state, {loading: true});
+            return {
+                ...state,
+                loading: true,
+            };
         case projectsConst.PROJECTS_LOADED:
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 data: [...action.projects],
                 loading: false,
                 loadingError: null,
-            });
+            };
         case projectsConst.PROJECTS_LOADING_ERROR:
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 loading: false,
                 loadingError: action.err,
-            });
+            };
         /*
-         * Adding
+         * Add
          */
         case projectsConst.ADD_PROJECT:
-            return Object.assign({}, state, {adding: true});
+            return {
+                ...state,
+                loading: false,
+                loadingError: action.err,
+            };
         case projectsConst.PROJECT_ADDED:
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 data: [
                     ...state.data,
                     action.project,
                 ],
                 adding: false,
                 addingError: null,
-            });
+            };
         case projectsConst.PROJECT_ADDING_ERROR:
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 adding: false,
                 addingError: action.err,
-            });
+            };
         /*
-         * Updating
+         * Update
          */
         case projectsConst.UPDATE_PROJECT:
-            return Object.assign({}, state, {updating: true});
+            return {
+                ...state,
+                updating: true,
+            };
         case projectsConst.PROJECT_UPDATED:
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 data: state.data.map((item) => {
-                    if (action.project.id === item.id) {
-                        return action.project;
-                    }
-                    return item;
+                    return action.project.id === item.id ? action.project : item;
                 }),
                 updating: false,
                 updatingError: null,
-            });
+            };
         case projectsConst.PROJECT_UPDATING_ERROR:
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 updating: false,
                 updatingError: action.err,
-            });
+            };
         /*
-         * Deleting
+         * Delete
          */
         case projectsConst.DELETE_PROJECT:
-            return Object.assign({}, state, {deleting: true});
+            return {
+                ...state,
+                deleting: true,
+            };
         case projectsConst.PROJECT_DELETED:
-            let projectsAfterDelete = state.data;
-            for (let i = 0, len = state.length; i < len; i++) {
-                if (state[i].id === action.id) {
-                    projectsAfterDelete = [
-                        ...state.slice(0, i),
-                        ...state.slice(i + 1),
-                    ];
-                }
-            }
-            return Object.assign({}, state, {
-                data: projectsAfterDelete,
+            return {
+                ...state,
+                data: state.data.filter(item => item.id !== action.id),
                 deleting: false,
                 deletingError: null,
-            });
+            };
         case projectsConst.PROJECT_DELETING_ERROR:
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 deleting: false,
                 deletingError: action.err,
-            });
+            };
         default:
             return state;
     }
