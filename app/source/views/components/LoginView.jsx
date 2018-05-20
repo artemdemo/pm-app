f30b2fbb7c69eba5302341eae185a6e9ab5e679c
Artem Demo <artemdemo@gmail.com>
2018-05-20 19:07:15 +0300

    updated bootstrap, added UserAuthForm

--- app/source/views/components/LoginView.jsx
+++ app/source/views/components/LoginView.jsx
@@ -1,84 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import UserAuthForm from '../../components/UserAuthForm/UserAuthForm';
 import { login } from '../../model/auth/authActions';
 import { errorMessage } from '../../model/notification/notificationActions';
 import User from '../../model/auth/User';
 import auth from '../../services/auth';
 import * as location from '../../services/location';
 
-import '../form-signin.less';
-
 class LoginView extends React.PureComponent {
-    constructor(props) {
-        super(props);
-
-        this.emailRef = null;
-        this.passwordRef = null;
-    }
-
     componentDidMount() {
         if (auth.isAuthorized()) {
             location.replace('/')
         }
     }
 
-    submitLogin(e) {
-        e.preventDefault();
+    submitLogin = ({ email, password }) => {
         const { login } = this.props;
         try {
             const loginUser = new User({
-                email: this.emailRef.value,
-                password: this.passwordRef.value,
+                email,
+                password,
             });
             login(loginUser);
         } catch (e) {
             errorMessage('Please fill all fields');
         }
-    }
+    };
 
     render() {
         return (
             <div className='container'>
-                <form
-                    className='form-signin'
-                    onSubmit={this.submitLogin.bind(this)}
+                <UserAuthForm
+                    onSubmit={this.submitLogin}
+                />
+                <Link
+                    to='/signup'
+                    className='btn btn-link btn-block'
                 >
-                    <h2 className='form-signin-heading'>Please sign in</h2>
-                    <label htmlFor='inputEmail' className='sr-only'>
-                        Email address
-                    </label>
-                    <input
-                        type='email'
-                        name='email'
-                        ref={ref => this.emailRef = ref}
-                        className='form-control form-signin__first-input'
-                        placeholder='Email address'
-                        required=''
-                        autoComplete='off'
-                    />
-                    <label htmlFor='inputPassword' className='sr-only'>
-                        Password
-                    </label>
-                    <input
-                        type='password'
-                        name='password'
-                        ref={ref => this.passwordRef = ref}
-                        className='form-control form-signin__last-input'
-                        placeholder='Password'
-                        required=''
-                        autoComplete='off'
-                    />
-                    <button
-                        className='btn btn-lg btn-primary btn-block'
-                        type='submit'
-                    >
-                        Login
-                    </button>
-                    <Link to='/signup' className='btn btn-link btn-block' data-qa='link-to-signup'>
-                        Create new account - sign up
-                    </Link>
-                </form>
+                    Create new account - sign up
+                </Link>
             </div>
         );
     }
