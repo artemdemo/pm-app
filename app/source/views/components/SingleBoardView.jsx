b9c2974caa111820ddd1237a9b59344672ceafe0
Artem Demo <artemdemo@gmail.com>
2018-06-08 11:47:25 +0300

    getCurrentBoard

--- app/source/views/components/SingleBoardView.jsx
+++ app/source/views/components/SingleBoardView.jsx
@@ -2,13 +2,52 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { createSelector } from 'reselect';
+import {
+    addBoard,
+} from '../../model/boards/boardsActions';
 import EntityModal from '../../components/EntityModal/EntityModal';
 
+const getCurrentBoard = createSelector(
+    props => props.boards.data,
+    props => props.params.boardId,
+    (boards, boardId) => {
+        return boards.find(item => String(item.id) === boardId);
+    },
+);
+
 class SingleBoardView extends React.PureComponent {
+    static getDerivedStateFromProps(props, state) {
+        const board = getCurrentBoard(props);
+        if (board && board !== state.prevProject) {
+            return {
+                name: board.name || '',
+                prevBoard: board,
+            };
+        }
+        return null;
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            prevBoard: null,
+        };
+    }
+
     render() {
         return (
             <EntityModal>
-                Single Scrum Board
+                <div className='form-group'>
+                    <input
+                        type='text'
+                        className='form-control'
+                        placeholder='Board name'
+                        onChange={e => this.setState({name: e.target.value})}
+                        value={this.state.name}
+                    />
+                </div>
             </EntityModal>
         );
     }
@@ -17,5 +56,7 @@ class SingleBoardView extends React.PureComponent {
 export default connect(
     state => ({
         boards: state.boards,
-    }),
+    }), {
+        addBoard,
+    },
 )(SingleBoardView);
