10f2e7b7600d50cad83c41dc5254a2d988d98449
Artem Demo <artemdemo@gmail.com>
2018-06-13 07:38:31 +0300

    fixed profile

--- app/source/views/components/ProfileView.jsx
+++ app/source/views/components/ProfileView.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const ProfileView = (props) => {
-    const { user } = props;
+    const { auth } = props;
 
     return (
-        <div className='list-container'>
-            <div className='list-container__list'>
-                <h4>Profile</h4>
-                <p>
-                    Username: {user.username}<br />
-                    Email: {user.email}
-                </p>
-            </div>
-        </div>
+        <React.Fragment>
+            <h4>Profile</h4>
+            <p>
+                Username: {auth.data.username}<br />
+                Email: {auth.data.email}
+            </p>
+        </React.Fragment>
     );
 };
 
 export default connect(
     state => ({
-        user: state.user,
+        auth: state.auth,
     })
 )(ProfileView);
