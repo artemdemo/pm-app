53e0eab1d94b0d53836701a56eea24f915553e12
Artem Demo <artemdemo@gmail.com>
2018-07-01 21:07:49 +0300

    select language in settings

--- app/source/views/components/SettingsView.jsx
+++ app/source/views/components/SettingsView.jsx
@@ -1,24 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SettingsView = () => {
-    return (
-        <React.Fragment>
-            <h4>Settings</h4>
-            <div className='row'>
-                <div className='col-md-4'>
-                    <div className='form-group'>
-                        <label htmlFor='languageInput'>Language</label>
-                        <select id='languageInput' className='form-control'>
-                            <option selected>En</option>
-                            <option>Ru</option>
-                        </select>
+class SettingsView extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.languages = [
+            {value: 'en', name: 'En'},
+            {value: 'ru', name: 'Ru'},
+        ];
+
+        this.state = {
+            selectedLng: this.languages[0].value,
+        };
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h4>Settings</h4>
+                <div className='row'>
+                    <div className='col-md-4'>
+                        <div className='form-group'>
+                            <label htmlFor='languageInput'>Language</label>
+                            <select
+                                id='languageInput'
+                                className='form-control'
+                                value={this.state.selectedLng}
+                                onChange={e => this.setState({ selectedLng: e.target.value })}
+                            >
+                                {this.languages.map(lng => (
+                                    <option
+                                        value={lng.value}
+                                        key={`languages-item-${lng.value}`}
+                                    >
+                                        {lng.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </React.Fragment>
-    );
-};
+            </React.Fragment>
+        );
+    }
+}
 
 export default connect(
     () => ({})
